4b4c926ecc125b8ef18cc07813818aee4fea60b6
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-01-14 12:32:58 +0530

    Refactor IconButton and Menu components

--- components/icon-button.tsx
+++ components/icon-button.tsx
@@ -1,13 +1,24 @@
+import { cn } from "@/lib/utils";
+
 const IconButton = ({
   title = "",
+  isActive = "",
   children,
 }: {
   title?: string;
+  isActive: string;
   children: React.ReactNode;
-}) => (
-  <div className="hover:bg-muted p-2 rounded transition cursor-pointer grid place-items-center">
-    <span title={title}>{children}</span>
-  </div>
-);
+}) => {
+  return (
+    <div
+      className={cn(
+        isActive === title ? "bg-muted" : "",
+        "hover:bg-muted p-2 rounded transition cursor-pointer grid place-items-center"
+      )}
+    >
+      <span title={title}>{children}</span>
+    </div>
+  );
+};
 
 export default IconButton;
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,26 +1,46 @@
+"use client";
+
 import { Download, Eraser, PencilLine, RedoDot, UndoDot } from "lucide-react";
+
 import IconButton from "./icon-button";
-import { Separator } from "./ui/separator";
+import React, { useState } from "react";
+
+const items = [
+  {
+    title: "Draw",
+    Icon: PencilLine,
+  },
+  {
+    title: "Eraser",
+    Icon: Eraser,
+  },
+  {
+    title: "Undo",
+    Icon: UndoDot,
+  },
+  {
+    title: "Redo",
+    Icon: RedoDot,
+  },
+  {
+    title: "Download",
+    Icon: Download,
+    size: 20,
+  },
+];
 
 const Menu = () => {
+  const [isActive, setIsActive] = useState("");
+
   return (
     <div className="h-full flex justify-center align-center gap-1 shadow-md border dark:bg-secondary px-1.5 py-1 rounded-xl">
-      <IconButton title="Draw">
-        <PencilLine size={20} />
-      </IconButton>
-      <IconButton title="Eraser">
-        <Eraser size={20} />
-      </IconButton>
-      <IconButton title="Undo">
-        <UndoDot size={20} />
-      </IconButton>
-      <IconButton title="Redo">
-        <RedoDot size={20} />
-      </IconButton>
-      <Separator orientation="vertical" className="bg-muted" />
-      <IconButton title="Download">
-        <Download size={20} />
-      </IconButton>
+      {items.map(({ Icon, title, size }) => (
+        <span key={title} onClick={() => setIsActive(title)}>
+          <IconButton title={title} isActive={isActive}>
+            <Icon size={size} />
+          </IconButton>
+        </span>
+      ))}
     </div>
   );
 };
