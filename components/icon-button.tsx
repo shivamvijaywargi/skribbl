54919a8ad7b9b07b1f066be1569cc2501d1cf062
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-01-14 10:35:54 +0530

    Update icon-button and menu components

--- components/icon-button.tsx
+++ components/icon-button.tsx
@@ -1,6 +1,12 @@
-const IconButton = ({ children }: { children: React.ReactNode }) => (
+const IconButton = ({
+  title = "",
+  children,
+}: {
+  title?: string;
+  children: React.ReactNode;
+}) => (
   <div className="hover:bg-muted p-2 rounded transition cursor-pointer grid place-items-center">
-    {children}
+    <span title={title}>{children}</span>
   </div>
 );
 
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,33 +1,25 @@
 import { Download, Eraser, PencilLine, RedoDot, UndoDot } from "lucide-react";
 import IconButton from "./icon-button";
+import { Separator } from "./ui/separator";
 
 const Menu = () => {
   return (
-    <div className="flex justify-center align-center gap-1 shadow-md border dark:bg-secondary px-1.5 py-1 rounded-xl">
-      <IconButton>
-        <span title="Draw">
-          <PencilLine size={20} />
-        </span>
+    <div className="h-full flex justify-center align-center gap-1 shadow-md border dark:bg-secondary px-1.5 py-1 rounded-xl">
+      <IconButton title="Draw">
+        <PencilLine size={20} />
       </IconButton>
-      <IconButton>
-        <span title="Eraser">
-          <Eraser size={20} />
-        </span>
+      <IconButton title="Eraser">
+        <Eraser size={20} />
       </IconButton>
-      <IconButton>
-        <span title="Undo">
-          <UndoDot size={20} />
-        </span>
+      <IconButton title="Undo">
+        <UndoDot size={20} />
       </IconButton>
-      <IconButton>
-        <span title="Redo">
-          <RedoDot size={20} />
-        </span>
+      <IconButton title="Redo">
+        <RedoDot size={20} />
       </IconButton>
-      <IconButton>
-        <span title="Download">
-          <Download size={20} />
-        </span>
+      <Separator orientation="vertical" className="bg-muted" />
+      <IconButton title="Download">
+        <Download size={20} />
       </IconButton>
     </div>
   );
