d587b874a31ddf7ab3426ab8aebd0b43d7f45993
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-03-23 17:18:51 +0530

    feat: add draw on canvas🎉

--- components/canvas-board.tsx
+++ components/canvas-board.tsx
@@ -11,6 +11,7 @@ const CanvasBoard = () => {
   const { color, size } = tools[activeMenuItem];
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const shouldDraw = useRef(false);
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -21,7 +22,51 @@ const CanvasBoard = () => {
     // Set canvas width and height on mount
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const handleMouseDown = (e: MouseEvent) => {
+      shouldDraw.current = true;
+
+      ctx?.beginPath();
+      ctx?.moveTo(e.clientX, e.clientY);
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!shouldDraw.current) return;
+
+      ctx?.lineTo(e.clientX, e.clientY);
+      ctx?.stroke();
+    };
+
+    const handleMouseUp = () => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const changeConfig = () => {
+      if (ctx) {
+        ctx.strokeStyle = color;
+        ctx.lineWidth = size;
+      }
+    };
+
+    changeConfig();
+  }, [color, size]);
 
   return <canvas ref={canvasRef}></canvas>;
 };
